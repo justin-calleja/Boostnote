3e560b7bc474fe78c58f38a5e1b4059ee9603bd3
Justin Calleja <calleja.justin@gmail.com>
2017-07-16 19:32:47 +0200

    Add active / focus and handling of key down to browser/components/StorageItem.js.

--- browser/components/StorageItem.js
+++ browser/components/StorageItem.js
@@ -8,6 +8,7 @@ import { isNumber } from 'lodash'
 
 /**
  * @param {boolean} isActive
+ * @param {boolean} isFocused
  * @param {Function} handleButtonClick
  * @param {Function} handleContextMenu
  * @param {string} folderName
@@ -17,42 +18,69 @@ import { isNumber } from 'lodash'
  * @param {Function} handleDrop
  * @param {Function} handleDragEnter
  * @param {Function} handleDragOut
+ * @param {Function} handleKeyDown
  * @return {React.Component}
  */
-const StorageItem = ({
-  isActive, handleButtonClick, handleContextMenu, folderName,
-  folderColor, isFolded, noteCount, handleDrop, handleDragEnter, handleDragLeave
-}) => (
-  <button styleName={isActive
-      ? 'folderList-item--active'
-      : 'folderList-item'
+class StorageItem extends React.Component {
+  componentDidUpdate () {
+    if (this.props.isFocused) {
+      this.button.focus()
     }
-    onClick={handleButtonClick}
-    onContextMenu={handleContextMenu}
-    onDrop={handleDrop}
-    onDragEnter={handleDragEnter}
-    onDragLeave={handleDragLeave}
-  >
-    <span styleName={isFolded
-      ? 'folderList-item-name--folded' : 'folderList-item-name'
-    }
-      style={{borderColor: folderColor}}
-    >
-      {isFolded ? folderName.substring(0, 1) : folderName}
-    </span>
-    {(!isFolded && isNumber(noteCount)) &&
-      <span styleName='folderList-item-noteCount'>{noteCount}</span>
-    }
-    {isFolded &&
-      <span styleName='folderList-item-tooltip'>
-        {folderName}
-      </span>
-    }
-  </button>
-)
+  }
+
+  render () {
+    const {
+      isActive,
+      handleButtonClick,
+      handleContextMenu,
+      folderName,
+      folderColor,
+      isFolded,
+      noteCount,
+      handleDrop,
+      handleDragEnter,
+      handleDragLeave,
+      isFocused,
+      handleKeyDown
+    } = this.props
+
+    const styleName = isFocused
+      ? 'folderList-item--active-focused'
+      : isActive ? 'folderList-item--active' : 'folderList-item'
+
+    return (
+      <button styleName={styleName}
+        ref={button => { this.button = button }}
+        onClick={handleButtonClick}
+        onContextMenu={handleContextMenu}
+        onDrop={handleDrop}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onKeyDown={handleKeyDown}
+      >
+        <span styleName={isFolded
+          ? 'folderList-item-name--folded' : 'folderList-item-name'
+        }
+          style={{ borderColor: folderColor }}
+        >
+          {isFolded ? folderName.substring(0, 1) : folderName}
+        </span>
+        {(!isFolded && isNumber(noteCount)) &&
+          <span styleName='folderList-item-noteCount'>{noteCount}</span>
+        }
+        {isFolded &&
+          <span styleName='folderList-item-tooltip'>
+            {folderName}
+          </span>
+        }
+      </button>
+    )
+  }
+}
 
 StorageItem.propTypes = {
   isActive: PropTypes.bool.isRequired,
+  isFocused: PropTypes.bool,
   handleButtonClick: PropTypes.func,
   handleContextMenu: PropTypes.func,
   folderName: PropTypes.string.isRequired,
@@ -60,7 +88,8 @@ StorageItem.propTypes = {
   isFolded: PropTypes.bool.isRequired,
   handleDragEnter: PropTypes.func.isRequired,
   handleDragLeave: PropTypes.func.isRequired,
-  noteCount: PropTypes.number
+  noteCount: PropTypes.number,
+  handleKeyDown: PropTypes.func
 }
 
 export default CSSModules(StorageItem, styles)
