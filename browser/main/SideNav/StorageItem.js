7143f926174d14ad8bce3f0bdfea6e1439024b93
Justin Calleja <calleja.justin@gmail.com>
2017-07-16 19:18:44 +0200

    Add SideNav/StorageItem.js changes to know when it's active v.s. focus.
    Also, openness is not kept as state but passed as props.

--- browser/main/SideNav/StorageItem.js
+++ browser/main/SideNav/StorageItem.js
@@ -8,6 +8,7 @@ import RenameFolderModal from 'browser/main/modals/RenameFolderModal'
 import dataApi from 'browser/main/lib/dataApi'
 import StorageItemChild from 'browser/components/StorageItem'
 import eventEmitter from 'browser/main/lib/eventEmitter'
+import { basePaths } from 'browser/lib/utils/paths'
 
 const { remote } = require('electron')
 const { Menu, MenuItem, dialog } = remote
@@ -16,8 +17,20 @@ class StorageItem extends React.Component {
   constructor (props) {
     super(props)
 
-    this.state = {
-      isOpen: true
+    this.handleToggleButtonClick = this.handleToggleButtonClick.bind(this)
+    this.handleHeaderInfoClick = this.handleHeaderInfoClick.bind(this)
+    this.handleHeaderContextMenu = this.handleHeaderContextMenu.bind(this)
+    this.handleAddFolderButtonClick = this.handleAddFolderButtonClick.bind(this)
+    this.folderToStorageItemChild = this.folderToStorageItemChild.bind(this)
+  }
+
+  componentDidUpdate () {
+    const { location, storage, focus } = this.props
+
+    const isActive = !!(location.pathname === basePaths.storages + storage.key)
+    const isFocused = isActive && focus.sideNav
+    if (isFocused) {
+      this.button.focus()
     }
   }
 
@@ -60,10 +73,8 @@ class StorageItem extends React.Component {
     }
   }
 
-  handleToggleButtonClick (e) {
-    this.setState({
-      isOpen: !this.state.isOpen
-    })
+  handleToggleButtonClick () {
+    this.props.toggleStorageOpenness(this.props.index)
   }
 
   handleAddFolderButtonClick (e) {
@@ -178,49 +189,56 @@ class StorageItem extends React.Component {
      })
   }
 
+  folderToStorageItemChild (folder) {
+    const { storage, location, isFolded, data, focus, handleKeyDown, dispatch } = this.props
+    const { folderNoteMap } = data
+
+    const isActive = !!(location.pathname === basePaths.storages + storage.key + basePaths.folders + folder.key)
+    const noteSet = folderNoteMap.get(storage.key + '-' + folder.key)
+    const isFocused = isActive && focus.sideNav
+    const noteCount = noteSet != null ? noteSet.size : 0
+
+    return (
+      <StorageItemChild
+        key={folder.key}
+        isActive={isActive}
+        isFocused={isFocused}
+        handleButtonClick={this.handleFolderButtonClick(folder.key)}
+        handleContextMenu={(e) => this.handleFolderButtonContextMenu(e, folder)}
+        folderName={folder.name}
+        folderColor={folder.color}
+        isFolded={isFolded}
+        noteCount={noteCount}
+        handleDrop={(e) => this.handleDrop(e, storage, folder, dispatch, location)}
+        handleDragEnter={this.handleDragEnter}
+        handleDragLeave={this.handleDragLeave}
+        handleKeyDown={handleKeyDown}
+      />
+    )
+  }
+
   render () {
-    let { storage, location, isFolded, data, dispatch } = this.props
-    let { folderNoteMap } = data
-    let folderList = storage.folders.map((folder) => {
-      let isActive = !!(location.pathname.match(new RegExp('\/storages\/' + storage.key + '\/folders\/' + folder.key)))
-      let noteSet = folderNoteMap.get(storage.key + '-' + folder.key)
+    const { storage, location, isFolded, focus, handleKeyDown, isOpen } = this.props
 
-      let noteCount = noteSet != null
-        ? noteSet.size
-        : 0
-      return (
-        <StorageItemChild
-          key={folder.key}
-          isActive={isActive}
-          handleButtonClick={(e) => this.handleFolderButtonClick(folder.key)(e)}
-          handleContextMenu={(e) => this.handleFolderButtonContextMenu(e, folder)}
-          folderName={folder.name}
-          folderColor={folder.color}
-          isFolded={isFolded}
-          noteCount={noteCount}
-          handleDrop={(e) => this.handleDrop(e, storage, folder, dispatch, location)}
-          handleDragEnter={this.handleDragEnter}
-          handleDragLeave={this.handleDragLeave}
-        />
-      )
-    })
+    const folderList = storage.folders.map(this.folderToStorageItemChild)
 
-    let isActive = location.pathname.match(new RegExp('\/storages\/' + storage.key + '$'))
+    const isActive = !!(location.pathname === basePaths.storages + storage.key)
+    const isFocused = isActive && focus.sideNav
+    const styleName = isFocused
+      ? 'header--active-focused'
+      : isActive ? 'header--active' : 'header'
 
     return (
       <div styleName={isFolded ? 'root--folded' : 'root'}
         key={storage.key}
       >
-        <div styleName={isActive
-            ? 'header--active'
-            : 'header'
-          }
-          onContextMenu={(e) => this.handleHeaderContextMenu(e)}
+        <div styleName={styleName}
+          onContextMenu={this.handleHeaderContextMenu}
         >
           <button styleName='header-toggleButton'
-            onMouseDown={(e) => this.handleToggleButtonClick(e)}
+            onMouseDown={this.handleToggleButtonClick}
           >
-            <i className={this.state.isOpen
+            <i className={isOpen
                 ? 'fa fa-caret-down'
                 : 'fa fa-caret-right'
               }
@@ -229,14 +247,16 @@ class StorageItem extends React.Component {
 
           {!isFolded &&
             <button styleName='header-addFolderButton'
-              onClick={(e) => this.handleAddFolderButtonClick(e)}
+              onClick={this.handleAddFolderButtonClick}
             >
               <i className='fa fa-plus' />
             </button>
           }
 
           <button styleName='header-info'
-            onClick={(e) => this.handleHeaderInfoClick(e)}
+            onClick={this.handleHeaderInfoClick}
+            ref={button => { this.button = button }}
+            onKeyDown={handleKeyDown}
           >
             <span styleName='header-info-name'>
               {isFolded ? storage.name.substring(0, 1) : storage.name}
@@ -248,7 +268,7 @@ class StorageItem extends React.Component {
             }
           </button>
         </div>
-        {this.state.isOpen &&
+        {isOpen &&
           <div styleName='folderList' >
             {folderList}
           </div>
@@ -259,7 +279,12 @@ class StorageItem extends React.Component {
 }
 
 StorageItem.propTypes = {
-  isFolded: PropTypes.bool
+  isFolded: PropTypes.bool,
+  focus: PropTypes.object,
+  handleKeyDown: PropTypes.func,
+  toggleStorageOpenness: PropTypes.func,
+  isOpen: PropTypes.bool,
+  index: PropTypes.number
 }
 
 export default CSSModules(StorageItem, styles)
