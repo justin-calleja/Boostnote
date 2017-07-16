50d2b6fd16614759807c9e470af8a8774200cfca
Justin Calleja <calleja.justin@gmail.com>
2017-07-16 19:34:30 +0200

    Add active / focus and handling of key down to browser/components/SideNavFilter.js.

--- browser/components/SideNavFilter.js
+++ browser/components/SideNavFilter.js
@@ -13,33 +13,72 @@ import styles from './SideNavFilter.styl'
  * @param {Function} handleStarredButtonClick
  * @return {React.Component}
  */
-const SideNavFilter = ({
-  isFolded, isHomeActive, handleAllNotesButtonClick,
-  isStarredActive, handleStarredButtonClick, isTrashedActive, handleTrashedButtonClick
-}) => (
-  <div styleName={isFolded ? 'menu--folded' : 'menu'}>
-    <button styleName={isHomeActive ? 'menu-button--active' : 'menu-button'}
-      onClick={handleAllNotesButtonClick}
-    >
-      <i className='fa fa-archive fa-fw' />
-      <span styleName='menu-button-label'>All Notes</span>
-    </button>
-    <button styleName={isStarredActive ? 'menu-button-star--active' : 'menu-button'}
-      onClick={handleStarredButtonClick}
-    >
-      <i className='fa fa-star fa-fw' />
-      <span styleName='menu-button-label'>Starred</span>
-    </button>
-    <button styleName={isTrashedActive ? 'menu-button--active' : 'menu-button'}
-      onClick={handleTrashedButtonClick}
-    >
-      <i className='fa fa-trash fa-fw' />
-      <span styleName='menu-button-label'>Trashed</span>
-    </button>
-  </div>
-)
+class SideNavFilter extends React.Component {
+  componentDidUpdate () {
+    const { isHomeActive, isStarredActive, focus } = this.props
+    if (focus.sideNav) {
+      if (isHomeActive) return this.homeButton.focus()
+      if (isStarredActive) return this.starredButton.focus()
+    }
+  }
+
+  render () {
+    const {
+      isFolded,
+      isHomeActive,
+      handleAllNotesButtonClick,
+      isStarredActive,
+      isTrashedActive,
+      handleStarredButtonClick,
+      handleTrashedButtonClick,
+      focus,
+      handleKeyDown
+    } = this.props
+
+    const homeStyleName = isHomeActive && focus.sideNav
+      ? 'menu-button--active-focused'
+      : isHomeActive ? 'menu-button--active' : 'menu-button'
+    const starredStyleName = isStarredActive && focus.sideNav
+      ? 'menu-button-star--active-focused'
+      : isStarredActive ? 'menu-button-star--active' : 'menu-button'
+
+    const trashedStyleName = isTrashedActive && focus.sideNav
+      ? 'menu-button-trash--active-focused'
+      : isTrashedActive ? 'menu-button-trash--active' : 'menu-button'
+
+    return (
+      <div styleName={isFolded ? 'menu--folded' : 'menu'}>
+        <button styleName={homeStyleName}
+          onClick={handleAllNotesButtonClick}
+          ref={button => { this.homeButton = button }}
+          onKeyDown={handleKeyDown}
+          >
+          <i className='fa fa-archive fa-fw' />
+          <span styleName='menu-button-label'>All Notes</span>
+        </button>
+        <button styleName={starredStyleName}
+          onClick={handleStarredButtonClick}
+          ref={button => { this.starredButton = button }}
+          onKeyDown={handleKeyDown}
+          >
+          <i className='fa fa-star fa-fw' />
+          <span styleName='menu-button-label'>Starred</span>
+        </button>
+        <button styleName={trashedStyleName}
+          onClick={handleTrashedButtonClick}
+          ref={button => { this.trashedButton = button }}
+          onKeyDown={handleKeyDown}
+        >
+          <i className='fa fa-trash fa-fw' />
+          <span styleName='menu-button-label'>Trashed</span>
+        </button>
+      </div>
+    )
+  }
+}
 
 SideNavFilter.propTypes = {
+  focus: PropTypes.object,
   isFolded: PropTypes.bool,
   isHomeActive: PropTypes.bool.isRequired,
   handleAllNotesButtonClick: PropTypes.func.isRequired,
